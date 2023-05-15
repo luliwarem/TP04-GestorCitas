df2906b919711294a31f322ad63d744ab4849ce2
Luciana Waremkraut <108527069+luliwarem@users.noreply.github.com>
2023-05-15 15:59:10 -0300

    Add files via upload

---
+++ src/citas.js
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function Citas() {
+    return (
+            <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            <div className="cita">
+              <p>Mascota: <span>Blacky</span></p>
+              <p>Dueño: <span>Jere</span></p>
+              <p>Fecha: <span>2023-05-08</span></p>
+              <p>Hora: <span>16:15</span></p>
+              <p>Sintomas: <span>No está comiendo</span></p><button className="button elimnar u-full-width">Eliminar ×</button>
+            </div>
+            <div className="cita">
+              <p>Mascota: <span>Nina</span></p>
+              <p>Dueño: <span>Martin</span></p>
+              <p>Fecha: <span>2021-08-05</span></p>
+              <p>Hora: <span>08:20</span></p>
+              <p>Sintomas: <span>Le duele la pierna</span></p><button className="button elimnar u-full-width">Eliminar ×</button>
+            </div>
+            <div className="cita">
+              <p>Mascota: <span>Sifon</span></p>
+              <p>Dueño: <span>Flecha</span></p>
+              <p>Fecha: <span>2023-06-10</span></p>
+              <p>Hora: <span>09:24</span></p>
+              <p>Sintomas: <span>Duerme mucho</span></p><button className="button elimnar u-full-width">Eliminar ×</button>
+            </div>            
+          </div>
+        );
+    }
+
+export default Citas;
---
+++ src/form.js
@@ -0,0 +1,40 @@
+import "./App.css";
+
+function Form() {
+  return (
+    <div >
+      <div className="one-half column">
+        <h2>Crear mi Cita</h2>
+        <form>
+          <label>Nombre Mascota</label>
+          <input
+            type="text"
+            name="mascota"
+            className="u-full-width"
+            placeholder="Nombre Mascota"
+            value=""
+          />
+          <label>Nombre Dueño</label>
+          <input
+            type="text"
+            name="propietario"
+            className="u-full-width"
+            placeholder="Nombre dueño de la mascota"
+            value=""
+          />
+          <label>Fecha</label>
+          <input type="date" name="fecha" className="u-full-width" value="" />
+          <label>hora</label>
+          <input type="time" name="hora" className="u-full-width" value="" />
+          <label>Sintomas</label>
+          <textarea name="sintomas" className="u-full-width"></textarea>
+          <button type="submit" className="u-full-width button-primary">
+            Agregar Cita
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
